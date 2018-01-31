a7035056225496d5bfc63884e32ff0f7454daa26
Jaeroine Mae Catacutan <35668936+jamiecatt@users.noreply.github.com>
2018-01-31 10:37:19 -0800

    Loops Workshop

---
+++ loops.js
@@ -0,0 +1,47 @@
+// Concentric Circles
+
+function yourPatch(x, y) {
+  noFill();
+  stroke(238);
+  rect(x, y, 480, 480);
+  
+for (var i = 100; i <= 400; i = i + 5) {
+  
+  // X, Y Coordinate On Canvas
+  
+  var startX = 200;
+  var startY = 200;
+  
+  // Changes Shape Size
+  var endX = i;
+  var endY = i;
+  ellipse(startX, startY, endX, endY);
+}}
+
+background(255);
+
+yourPatch(0, 0);
+yourPatch(width-480, height-480);
+
+// Taller Lines
+
+
+function yourPatch(x, y) {
+  noFill();
+  stroke(238);
+  rect(x, y, 480, 480);
+  
+for (var i = 20; i <= 400; i = i + 10) {
+  var startX = 20;
+  var startY = i;
+  var endX = i + 20;
+  var endY = i;
+  line(startX, startY, endX, endY);
+  
+}
+}
+
+background(255);
+
+yourPatch(0, 0);
+yourPatch(width-480, height-480);
