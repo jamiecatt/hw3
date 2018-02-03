8f3e811876a0bd491f33374c97cef77103c82ce0
Jaeroine Mae Catacutan <35668936+jamiecatt@users.noreply.github.com>
2018-02-03 00:07:29 -0800

    Cone

--- cone-of-lines.js
+++ cone-of-lines.js
@@ -1,1 +1,10 @@
+// Lines: Alternative Code (Counter)
 
+var counter = 10;
+while (counter <= width) { 
+  line (counter, 10, 10, counter);
+  counter = counter + 5;
+}
+
+// Paremeters: (x, y, x, y)
+// Increments: 5; x1 = y2; y1 = x2
