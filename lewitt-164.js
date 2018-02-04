3436230d46f425b6dba62112530867e04f373a7d
Jaeroine Mae Catacutan <35668936+jamiecatt@users.noreply.github.com>
2018-02-04 04:05:23 -0800

    LeWitt 164

---
+++ lewitt-164.js
@@ -0,0 +1,14 @@
+function setup() { 
+  createCanvas(400, 400);
+} 
+
+function draw() { 
+background (255);
+	stroke ('black');
+	rect (50, 50, 300, 300);
+	
+	stroke ('red');
+	line (150, 250, 250, 150);
+	line (100, 200, 300, 200);
+
+}
