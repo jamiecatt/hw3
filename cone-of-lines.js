bc8f145c9f0e6abac7bbd028c638494b37163ae7
Jaeroine Mae Catacutan <35668936+jamiecatt@users.noreply.github.com>
2018-02-04 02:09:59 -0800

    Cone

--- cone-of-lines.js
+++ cone-of-lines.js
@@ -1,4 +1,37 @@
-// Lines: Alternative Code (Counter)
+// Cone of Lines
+
+function setup() { 
+  createCanvas(400, 400);
+} 
+
+function draw() { 
+  background(220);
+	
+	
+function jmgcPatch(x, y) {
+  noFill();
+  stroke(238);
+  rect(x, y, 480, 480);
+  
+for (var i = 20; i <= 400; i = i + 10) {
+  var startX = 240;
+  var startY = i;
+  var endX = 20;
+  var endY = 240;
+  line(startX, startY, endX, endY);
+  
+}
+}
+
+background(255);
+
+jmgcPatch(0, 0);
+jmgcPatch(width-480, height-480);
+
+}
+
+
+// Lines: Alternative Code (Counter) - Notes In Class
 
 var counter = 10;
 while (counter <= width) { 
@@ -6,5 +39,5 @@ while (counter <= width) {
   counter = counter + 5;
 }
 
-// Paremeters: (x, y, x, y)
+// Parameters: (x, y, x, y)
 // Increments: 5; x1 = y2; y1 = x2
